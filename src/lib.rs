bd9ed8e20f26cb9ab6e2dc3a8a475669705e4aa7
J. Cliff Dyer <jcd@sdf.org>
2019-06-25 20:31:08 -0400

    Implement log compaction

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
-use std::collections::{BTreeMap};
+use std::collections::BTreeMap;
 use std::fs::{create_dir_all, File, OpenOptions};
 use std::io::{BufReader, BufWriter, SeekFrom, prelude::*};
 use std::path::{Path, PathBuf};
-
 
 use failure::{format_err, Error};
 use serde::{Deserialize, Serialize};
@@ -19,7 +18,7 @@ pub struct Cavey {
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all="snake_case")]
+#[serde(rename_all = "snake_case")]
 pub enum Command {
     Put { key: String, value: String },
     Remove { key: String },
@@ -31,7 +30,6 @@ impl Cavey {
     }
 
     pub fn open<P: AsRef<Path>>(path: P) -> Result<Cavey> {
-
         let datadir = path.as_ref().join("data");
         create_dir_all(&datadir)?;
         let candidates = std::fs::read_dir(&datadir)?
@@ -39,19 +37,20 @@ impl Cavey {
             .collect::<std::io::Result<Vec<_>>>();
         let mut candidates = candidates?;
         candidates.sort();
-        let filename = candidates.into_iter().take(1).next().unwrap_or_else(|| datadir.join(&format!("{:08}", 0)));
-        let file_version = usize::from_str_radix(&filename.file_name().unwrap().to_string_lossy(), 0x10)?;
-        let mut file = BufWriter::new(
-            OpenOptions::new()
-                .create(true)
-                .append(true)
-                .open(&filename)?
-        );
+        let filename = candidates
+            .into_iter()
+            .take(1)
+            .next()
+            .unwrap_or_else(|| datadir.join(&format!("{:08}", 0)));
+        let file_version =
+            usize::from_str_radix(&filename.file_name().unwrap().to_string_lossy(), 0x10)?;
+        let mut file = BufWriter::new(OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open(&filename)?);
         file.seek(SeekFrom::End(0))?;
 
-        let mut reader = OpenOptions::new()
-            .read(true)
-            .open(&filename)?;
+        let mut reader = OpenOptions::new().read(true).open(&filename)?;
 
         let mut keymap = BTreeMap::new();
         let mut offset = reader.seek(SeekFrom::Start(0))?;
@@ -71,14 +70,22 @@ impl Cavey {
             offset += line.len() as u64 + 1;
             entries += 1;
         }
-        Ok(Cavey { datadir, file, keymap, entries, file_version })
+        Ok(Cavey {
+            datadir,
+            file,
+            keymap,
+            entries,
+            file_version,
+        })
     }
 
     pub fn get(&mut self, key: String) -> Result<Option<String>> {
         if let Some((filename, offset)) = self.keymap.get(&key) {
             let mut reader = File::open(filename)?;
             reader.seek(SeekFrom::Start(*offset))?;
-            let cmd = serde_json::Deserializer::from_reader(&mut reader).into_iter().next();
+            let cmd = serde_json::Deserializer::from_reader(&mut reader)
+                .into_iter()
+                .next();
             match cmd {
                 Some(Ok(Command::Put { value, .. })) => Ok(Some(value)),
                 Some(Ok(Command::Remove { .. })) => Err(format_err!("unexpected remove")),
@@ -118,16 +125,21 @@ impl Cavey {
     }
 
     fn should_compact(&mut self) -> bool {
-        (self.entries >= 500) && (self.entries > (3 * self.keymap.len()))
+        (self.entries >= 500) && (self.entries > (10 * self.keymap.len()))
     }
 
     fn compact(&mut self) -> Result<()> {
+        // Collect old log file(s).
         let existing = std::fs::read_dir(&self.datadir)?.collect::<Vec<_>>();
+
+        // Read out commands from old log file(s).
         let mut commands = Vec::with_capacity(self.keymap.len());
         for (_, (filename, offset)) in self.keymap.iter() {
             let mut f = File::open(filename)?;
             f.seek(SeekFrom::Start(*offset))?;
-            let cmd = serde_json::Deserializer::from_reader(&mut f).into_iter().next();
+            let cmd = serde_json::Deserializer::from_reader(&mut f)
+                .into_iter()
+                .next();
             if let Some(Ok(cmd)) = cmd {
                 // TODO: Validate command was Put
                 if let Command::Put { .. } = &cmd {
@@ -139,10 +151,15 @@ impl Cavey {
             }
         }
 
-        self.file_version += 1;
+        // Clear in-memory map
         self.keymap.clear();
+
+        // Create new file
+        self.file_version += 1;
         let newfilename = self.current_file();
         let mut w = BufWriter::new(File::create(&newfilename)?);
+
+        // Read commands into new file, and populate in-memory map with new locations
         for cmd in commands {
             let offset = w.seek(SeekFrom::Current(0))?;
             serde_json::to_writer(&mut w, &cmd)?;
@@ -153,13 +170,16 @@ impl Cavey {
         }
         w.flush()?;
 
-        std::mem::swap(&mut self.file, &mut w);
-        drop(w);
-        self.entries = self.keymap.len();
+        // Make the new file the primary log.
+        self.file = w;
+
+        // Remove the old file(s)
         for direntry in existing {
             std::fs::remove_file(direntry?.path())?;
         }
+
+        // Reset the entry count
+        self.entries = self.keymap.len();
         Ok(())
     }
-
 }
